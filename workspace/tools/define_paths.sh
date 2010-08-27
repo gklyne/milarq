6fa7e375e3465d8f40bc172875156cae95f4ce1e
graham <graham@localhost>
2010-08-27 13:24:08 +0100

    Changes for running sed in MacOS environment Remove TDB warning messages from output of tdbquery

--- workspace/tools/define_paths.sh
+++ workspace/tools/define_paths.sh
@@ -15,16 +15,31 @@
 #   CP             - classpath for MILARQ utilities
 #   JAVA           - command to activiate Java environment with classpath, options, etc.
 #
-# Also defines function classpath for use by the "process..." scripts
+# Also defines function classpath for use by the "process..." scripts,
+# and commands for tdbquery, tdbloader, tdblarqindex
 #
 
-# Can override by setting JAVA_BIN externally
-# Generic
-### JAVA_BIN=${JAVA_BIN:-java}
-# MacOS
-JAVA_BIN=${JAVA_BIN:-/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands/java}
-# Linux
-### JAVA_BIN=${JAVA_BIN:-/usr/lib/jvm/java-6-sun/jre/bin/java}
+# Can override the following by setting JAVA_BIN externally
+SYSTEM="$(uname -s)"
+if [ "$SYSTEM" == "Linux" ]; then
+    # Linux
+    PATHSEP=':'
+    SEDE="sed -r"
+    JAVA_BIN=${JAVA_BIN:-java}
+elif [ "$SYSTEM" == "Darwin" ]; then
+    # MacOS
+    PATHSEP=':'
+    SEDE="sed -E"
+    JAVA_BIN=${JAVA_BIN:-/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands/java}
+elif [ "$SYSTEM" == "Cygwin" ]; then
+    # Windows+Cygwin
+    PATHSEP=';'
+    SEDE="sed -r" # TODO: is this right?
+    JAVA_BIN=${JAVA_BIN:-java}
+else
+    echo "Unrecognized system type: $SYSTEM"
+    exit 1
+fi
 
 # Can override by setting WORKSPACE_ROOT externally
 WORKSPACE_ROOT=${WORKSPACE_ROOT:-~/workspace}
@@ -35,7 +50,7 @@ MILARQ_ROOT=${WORKSPACE_ROOT}/${MILARQ_PROJECT}
 
 # Derived values
 MILARQ_CLASSES=$MILARQ_ROOT/webapp/WEB-INF/classes
-MILARQ_LIBS="`echo $MILARQ_ROOT/webapp/WEB-INF/lib/*.jar | sed -e 's/ /:/g'`"
+MILARQ_LIBS="`echo $MILARQ_ROOT/webapp/WEB-INF/lib/*.jar | $SEDE 's/ /:/g'`"
 CP=$MILARQ_CLASSES:$MILARQ_LIBS
 JAVA="$JAVA_BIN -Xmx1024M -server -cp $CP"
 
