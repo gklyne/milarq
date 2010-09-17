0f351a6f697f5c679bc623ad96833601627e48a3
Chris Dollin <ehog.hedge@gmail.com>
2010-09-17 14:10:13 +0100

    allow subejcts to be fixed.

--- src/propertyfunctions/genericIndex.java
+++ src/propertyfunctions/genericIndex.java
@@ -18,8 +18,7 @@ import com.hp.hpl.jena.shared.WrappedIOException;
 import com.hp.hpl.jena.sparql.core.Var;
 import com.hp.hpl.jena.sparql.engine.ExecutionContext;
 import com.hp.hpl.jena.sparql.engine.QueryIterator;
-import com.hp.hpl.jena.sparql.engine.binding.Binding;
-import com.hp.hpl.jena.sparql.engine.binding.Binding1;
+import com.hp.hpl.jena.sparql.engine.binding.*;
 import com.hp.hpl.jena.sparql.pfunction.PropFuncArg;
 import com.hp.hpl.jena.sparql.pfunction.PropFuncArgType;
 import com.hp.hpl.jena.sparql.pfunction.PropertyFunctionEval;
@@ -73,85 +72,13 @@ public class genericIndex extends PropertyFunctionEval
         ) 
         {
         List<Node> objects = asList( argObject );
-        final Var VS = Var.alloc(argSubject.getArg()); 
+        final Node subject = argSubject.getArg();
         final List<Var> vars = new ArrayList<Var>();
         for (Node v: objects.subList( 1, objects.size() ) ) vars.add( Var.alloc( v ) );
-        return new QueryIterator() 
-            {
-            BufferedReader in = inFromFile( indexFullName );
-            String current = readLine( in );
-                        
-            @Override public void abort() 
-                { close(); }
-
-            @Override public Binding nextBinding() 
-                { return next(); }
-
-            @Override public void close() 
-                { 
-                try { in.close(); }
-                catch (IOException e) { throw new WrappedIOException( e ); }
-                }
-
-            @Override public boolean hasNext() 
-                { return current != null; }
-
-            @Override public Binding next() 
-                {            
-                int i = 0;
-                String [] elements = current.split( "," );
-                Node S = stringToNode( elements[i++] );
-                Binding b = new Binding1( binding, VS, S );
-                for (Var v: vars) b = new Binding1( b, v, stringToNode( elements[i++] ) );
-                current = readLine( in );
-                if (current == null) close();
-                ////TODO: remove? System.err.println( ">> gi: returning " + b );
-                return b;
-                }
-
-            private Node stringToNode( String s )
-                {
-                String spelling = s.replaceAll( "\\\\C", "," );
-                if (spelling.startsWith( "<" ))
-                    return Node.createURI( spelling.substring( 1, spelling.length() - 1 ) );
-                else if (spelling.matches( "^-?[0-9].*" ))
-                    return Node.createLiteral( LiteralLabelFactory.create( Integer.parseInt( spelling ) ) );
-                else
-                    {
-                    int quote = spelling.lastIndexOf( '"' );
-                    String lf = spelling.substring( 1, quote );
-                    if (quote == spelling.length() - 1) return Node.createLiteral( lf );
-                    else if (spelling.charAt( quote + 1 ) == '@') return Node.createLiteral( lf, spelling.substring( quote + 2 ), false );
-                    else
-                        {
-                        RDFDatatype dt = new BaseDatatype( spelling.substring( quote + 2 ) );
-                        LiteralLabel ll = LiteralLabelFactory.createLiteralLabel( lf, "", dt );
-                        return Node.createLiteral( ll );
-                        }
-                    }
-                }
-
-            @Override public void remove() 
-                {
-                throw new NotImplemented( "oops" );
-                }
-
-            @Override public void output(IndentedWriter out, SerializationContext sCxt) 
-                {
-                // TODO Auto-generated method stub
-                }
-
-            @Override public String toString(PrefixMapping pmap) 
-                {
-                // TODO Auto-generated method stub
-                return null;
-                }
-
-            @Override public void output(IndentedWriter out) 
-                {
-                // TODO Auto-generated method stub
-                }
-            };
+        return subject.isVariable()
+            ? new GenericIndexIteratorSubjectVariable( binding, vars, subject )
+            : new GenericIndexIteratorSubjectBound( binding, vars, subject )
+            ;
         }
 
     private String readLine(BufferedReader in) 
@@ -177,6 +104,139 @@ public class genericIndex extends PropertyFunctionEval
             }
         }
     
+    
+    private class GenericIndexIterator
+        {
+        protected final Binding binding;
+        protected final List<Var> vars;
+        protected final Node subject;
+        
+        protected final BufferedReader in = inFromFile( indexFullName );
+        
+        protected String current = readLine( in );
+
+        private GenericIndexIterator( Binding binding, List<Var> vars, Node subject )
+            {
+            this.binding = binding;
+            this.vars = vars;
+            this.subject = subject;
+            }
+
+        public void abort() 
+            { close(); }
+
+        public void close() 
+            { 
+            try { in.close(); }
+            catch (IOException e) { throw new WrappedIOException( e ); }
+            }
+        
+        protected Node stringToNode( String s )
+            {
+            String spelling = s.replaceAll( "\\\\C", "," );
+            if (spelling.startsWith( "<" ))
+                return Node.createURI( spelling.substring( 1, spelling.length() - 1 ) );
+            else if (spelling.matches( "^-?[0-9].*" ))
+                return Node.createLiteral( LiteralLabelFactory.create( Integer.parseInt( spelling ) ) );
+            else
+                {
+                int quote = spelling.lastIndexOf( '"' );
+                String lf = spelling.substring( 1, quote );
+                if (quote == spelling.length() - 1) return Node.createLiteral( lf );
+                else if (spelling.charAt( quote + 1 ) == '@') return Node.createLiteral( lf, spelling.substring( quote + 2 ), false );
+                else
+                    {
+                    RDFDatatype dt = new BaseDatatype( spelling.substring( quote + 2 ) );
+                    LiteralLabel ll = LiteralLabelFactory.createLiteralLabel( lf, "", dt );
+                    return Node.createLiteral( ll );
+                    }
+                }
+            }
+
+        public void remove() 
+            {
+            throw new NotImplemented( "oops" );
+            }
+
+        public void output(IndentedWriter out, SerializationContext sCxt) 
+            {
+            // TODO Auto-generated method stub
+            }
+
+        public String toString(PrefixMapping pmap) 
+            {
+            // TODO Auto-generated method stub
+            return null;
+            }
+
+        public void output(IndentedWriter out) 
+            {
+            // TODO Auto-generated method stub
+            }
+        }
+    
+    private final class GenericIndexIteratorSubjectBound extends GenericIndexIterator implements QueryIterator
+        {
+        private GenericIndexIteratorSubjectBound( Binding binding, List<Var> vars, Node subject )
+            { super( binding, vars, subject ); }
+
+        @Override public Binding nextBinding() 
+            { return next(); }
+
+        public boolean hasNext() 
+            {
+            while (true)
+                {
+                if (current == null) { close(); return false; }
+                if (stringToNode(current.split(",")[0]).equals(subject)) return true;
+                readAndCloseIfDone();
+                }
+            }
+        
+        @Override public Binding next() 
+            {            
+            if (!hasNext()) throw new NoSuchElementException();
+            int i = 0;
+            String [] elements = current.split( "," );
+            Binding b = new Binding0();
+            for (Var v: vars) b = new Binding1( b, v, stringToNode( elements[i++] ) );
+            readAndCloseIfDone();
+            return b;
+            }
+
+        private void readAndCloseIfDone()
+            {
+            current = readLine( in );
+            if (current == null) close();
+            }
+        }
+    
+    private final class GenericIndexIteratorSubjectVariable extends GenericIndexIterator implements QueryIterator
+        {
+        private GenericIndexIteratorSubjectVariable( Binding binding, List<Var> vars, Node subject )
+            { super( binding, vars, subject ); }
+
+        @Override public Binding nextBinding() 
+            { return next(); }
+        
+        public boolean hasNext() 
+            { return current != null; }
+        
+        @Override public Binding next() 
+            {            
+            int i = 0;
+            String [] elements = current.split( "," );
+            Node S = stringToNode( elements[i++] );
+            final Var VS = Var.alloc(subject); 
+            Binding b = new Binding1( binding, VS, S );
+            for (Var v: vars) b = new Binding1( b, v, stringToNode( elements[i++] ) );
+            current = readLine( in );
+            if (current == null) close();
+            ////TODO: remove? System.err.println( ">> gi: returning " + b );
+            return b;
+            }
+        }
+
     static class EmptyReader extends Reader
         {
         @Override public int read( char[] cbuf, int off, int len ) 
